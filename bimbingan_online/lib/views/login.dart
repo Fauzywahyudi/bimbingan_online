d5d60685012008076f165d1521636277151dc6c5
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-08-27 00:13:18 +0700

    add page login mahasiswa dan dosen #1

---
+++ bimbingan_online/lib/views/login.dart
@@ -0,0 +1,231 @@
+import 'package:bimbingan_online/models/page_login.dart';
+import 'package:bimbingan_online/views/register.dart';
+import 'package:flutter/material.dart';
+import 'package:wave/config.dart';
+import 'package:wave/wave.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  var _tecNim = TextEditingController();
+  var _tecPass = TextEditingController();
+  bool get _isMahasiswa => _pageLogin == PageLogin.isMahasiswa ? true : false;
+  PageLogin _pageLogin = PageLogin.isMahasiswa;
+
+  void _changePage(PageLogin page) {
+    if (page == PageLogin.isMahasiswa) {
+      if (!_isMahasiswa) {
+        setState(() {
+          _pageLogin = PageLogin.isMahasiswa;
+        });
+      }
+    } else {
+      if (_isMahasiswa) {
+        setState(() {
+          _pageLogin = PageLogin.isDosen;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: 650,
+            child: RotatedBox(
+              quarterTurns: 2,
+              child: WaveWidget(
+                config: CustomConfig(
+                  gradients: [
+                    [Colors.deepPurple, Colors.deepPurple.shade200],
+                    [Colors.indigo.shade200, Colors.purple.shade200],
+                  ],
+                  durations: [19440, 10800],
+                  heightPercentages: [0.20, 0.25],
+                  blur: MaskFilter.blur(BlurStyle.solid, 10),
+                  gradientBegin: Alignment.bottomLeft,
+                  gradientEnd: Alignment.topRight,
+                ),
+                waveAmplitude: 0,
+                size: Size(
+                  double.infinity,
+                  double.infinity,
+                ),
+              ),
+            ),
+          ),
+          ListView(
+            children: <Widget>[
+              Container(
+                height: 400,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Login",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.white70,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28.0)),
+                    Card(
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 30),
+                      elevation: 11,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      child: TextField(
+                        controller: _tecNim,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Colors.black26,
+                            ),
+                            hintText: "${_isMahasiswa ? "NIM" : "NIDN"}",
+                            hintStyle: TextStyle(color: Colors.black26),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0)),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 16.0)),
+                      ),
+                    ),
+                    Card(
+                      margin: EdgeInsets.only(left: 30, right: 30, top: 20),
+                      elevation: 11,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      child: TextField(
+                        controller: _tecPass,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Colors.black26,
+                            ),
+                            hintText: "Password",
+                            hintStyle: TextStyle(
+                              color: Colors.black26,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40.0)),
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 16.0)),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.all(30.0),
+                      child: RaisedButton(
+                        padding: EdgeInsets.symmetric(vertical: 16.0),
+                        color: Colors.pink,
+                        onPressed: () {},
+                        elevation: 11,
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(40.0))),
+                        child: Text("Login",
+                            style: TextStyle(color: Colors.white70)),
+                      ),
+                    ),
+                    // Text("Forgot your password?",
+                    //     style: TextStyle(color: Colors.white))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Text("Login sebagai..."),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Expanded(
+                          child: RaisedButton(
+                            child: Text("Mahasiswa"),
+                            textColor: Colors.white,
+                            color: _isMahasiswa
+                                ? Colors.deepPurple
+                                : Colors.deepPurple[100],
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40)),
+                            ),
+                            onPressed: () => _changePage(PageLogin.isMahasiswa),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Expanded(
+                          child: RaisedButton(
+                            child: Text("Dosen"),
+                            textColor: Colors.white,
+                            color: !_isMahasiswa
+                                ? Colors.deepPurple
+                                : Colors.deepPurple[100],
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40)),
+                            ),
+                            onPressed: () => _changePage(PageLogin.isDosen),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                      ],
+                    ),
+                    _isMahasiswa
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Belum punya akun?"),
+                              FlatButton(
+                                child: Text("Sign up"),
+                                textColor: Colors.indigo,
+                                onPressed: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Register(
+                                      pageLogin: PageLogin.isMahasiswa,
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        : Container()
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
