53d23295f682ceadff5e54c052a1e66a85568915
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-08-27 15:13:46 +0700

    #7 rev Register Mahasiswa  and add asset

---
+++ bimbingan_online/lib/utils/assets/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+var colPrimary = Colors.deepPurple;
+var colSecodary = Colors.pink;
+var colLight = Colors.white;
+var colDark = Colors.black87;
+var colBlack = Colors.black;
+var colHint = Colors.black26;
+var colValue = Colors.black54;
+var colInfo = Colors.lightBlue;
+var colSuccess = Colors.lightGreen;
+var colDanger = Colors.red[400];
