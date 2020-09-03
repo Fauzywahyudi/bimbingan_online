91d62eedf824623ddbf5f3df9a9d0979bf54ec35
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-09-03 15:33:19 +0700

    #29 add ConfirmPage Dosen dan Fungsi Konfirmasi dan Reject

--- backend_bimbingan/dosen/kaprodi/konfirmasiAkun.php
+++ backend_bimbingan/dosen/kaprodi/konfirmasiAkun.php
@@ -13,22 +13,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 function Dosen()
 {
     require '../../koneksi.php';
-    $data = array();
 
-    $sql = $kon->query("SELECT * FROM mahasiswa WHERE status='$status' ORDER BY nim_mahasiswa ASC");
-    while ($fetchData = $sql->fetch_array()) {
-        $data[] = $fetchData;
-    }
-
-    if (isset($data)) {
-        $response['value'] = 1;
-        $response['message'] = "Load data berhasil";
-        $response['data'] = json_encode($data);
-        echo json_encode($response);
+    $status = $_POST['status'];
+    $id = $_POST['id'];
+    if ($status == "Confirm") {
+        $sql = $kon->query("UPDATE `dosen` SET `status`='Confirm' WHERE id_dosen='$id'");
+        if ($sql) {
+            $response['value'] = 1;
+            $response['message'] = "Konfirmasi berhasil";
+            echo json_encode($response);
+        } else {
+            $response['value'] = 0;
+            $response['message'] = "Konfirmasi gagal";
+            echo json_encode($response);
+        }
     } else {
-        $response['value'] = 0;
-        $response['message'] = "Load data gagal";
-        echo json_encode($response);
+        $sql = $kon->query("UPDATE `dosen` SET `status`='Reject' WHERE id_dosen='$id'");
+        if ($sql) {
+            $response['value'] = 1;
+            $response['message'] = "Reject berhasil";
+            echo json_encode($response);
+        } else {
+            $response['value'] = 0;
+            $response['message'] = "Reject gagal";
+            echo json_encode($response);
+        }
     }
 }
 
--- bimbingan_online/lib/views/dosen/pages/fitur_kaprodi/dosen/comfirm_dosen.dart
+++ bimbingan_online/lib/views/dosen/pages/fitur_kaprodi/dosen/comfirm_dosen.dart
@@ -2,20 +2,20 @@ import 'package:bimbingan_online/providers/konfirmasi_provider.dart';
 import 'package:bimbingan_online/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class ConfirmMahasiswa extends StatefulWidget {
+class ConfirmDosen extends StatefulWidget {
   final data;
 
-  const ConfirmMahasiswa({Key key, this.data}) : super(key: key);
+  const ConfirmDosen({Key key, this.data}) : super(key: key);
   @override
-  _ConfirmMahasiswaState createState() => _ConfirmMahasiswaState();
+  _ConfirmDosenState createState() => _ConfirmDosenState();
 }
 
-class _ConfirmMahasiswaState extends State<ConfirmMahasiswa> {
+class _ConfirmDosenState extends State<ConfirmDosen> {
   KonfirmasiProvider _konfirmasiProvider = KonfirmasiProvider();
 
   void _konfirmasi(String status) async {
     await _konfirmasiProvider.konfirmasiAktor(
-        context, int.parse(widget.data['id_mahasiswa']), status, "Mahasiswa");
+        context, int.parse(widget.data['id_dosen']), status, "Dosen");
     popPage(context);
   }
 
@@ -23,18 +23,20 @@ class _ConfirmMahasiswaState extends State<ConfirmMahasiswa> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.data['nama_mahasiswa']),
+        title: Text(widget.data['nama_dosen']),
       ),
       body: Container(
         child: SingleChildScrollView(
           child: Column(
             children: [
               _buildListTile(
-                  widget.data['nim_mahasiswa'], "NIM Mahasiswa", Icons.person),
-              _buildListTile(widget.data['nama_mahasiswa'], "Nama Mahasiswa",
+                  widget.data['nidn_dosen'], "NIDN Dosen", Icons.person),
+              _buildListTile(
+                  widget.data['nama_dosen'] + " " + widget.data['gelar'],
+                  "Nama Dosen",
                   Icons.person),
               _buildListTile(widget.data['jk'], "Jenis Kelamin", Icons.wc),
-              _buildListTile(widget.data['jurusan'], "Jurusan", Icons.school),
+              _buildListTile(widget.data['jabatan'], "Jabatan", Icons.school),
               _buildListTile(widget.data['alamat'], "Alamat", Icons.home),
               _buildListTile(widget.data['nohp'], "No. Handphone", Icons.call),
               _buildListTile(widget.data['tgl_daftar'], "Tanggal Daftar",
