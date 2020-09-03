3b7464cbba78c286773521cf287fc2b9f99d525f
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-09-03 12:58:43 +0700

    #24 add List Mahasiswa Confirm dan Pending di Ketua Prodi

---
+++ bimbingan_online/lib/providers/mahasiswa_provider.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:bimbingan_online/models/mahasiswa.dart';
+import 'package:bimbingan_online/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:bimbingan_online/utils/link.dart' as link;
+
+class MahasiswaProvider {
+  Future<List> getMahasiswa(BuildContext context, String status) async {
+    final result =
+        await http.post(link.Link.kaprodi + "getMahasiswa.php", body: {
+      "status": status,
+    });
+    final response = await json.decode(result.body);
+    List data;
+    int value = response['value'];
+    String pesan = response['message'];
+    if (value == 1) {
+      data = json.decode(response['data']);
+    } else {
+      messageDanger(context, pesan);
+    }
+    return data;
+  }
+
+  Future<List> getMahasiswaById(BuildContext context, int id) async {
+    final result =
+        await http.post(link.Link.kaprodi + "getMahasiswaById.php", body: {
+      "id": id.toString(),
+    });
+    final response = await json.decode(result.body);
+    List data;
+    int value = response['value'];
+    String pesan = response['message'];
+    if (value == 1) {
+      data = json.decode(response['data']);
+    } else {
+      messageDanger(context, pesan);
+    }
+    return data;
+  }
+}
---
+++ bimbingan_online/lib/views/dosen/pages/fitur_kaprodi/mahasiswa/mahasiswa_page.dart
@@ -0,0 +1,122 @@
+import 'package:bimbingan_online/providers/mahasiswa_provider.dart';
+import 'package:bimbingan_online/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MahasiswaPage extends StatefulWidget {
+  static const routeName = '/MahasiswaPage';
+  @override
+  MahasiswaPageState createState() => MahasiswaPageState();
+}
+
+class MahasiswaPageState extends State<MahasiswaPage>
+    with TickerProviderStateMixin {
+  TabController tabController;
+
+  MahasiswaProvider _mahasiswaProvider = MahasiswaProvider();
+
+  Future<List> _getMahasiswa(String status) async {
+    final result = await _mahasiswaProvider.getMahasiswa(context, status);
+    return result;
+  }
+
+  @override
+  void initState() {
+    tabController = new TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mahasiswa"),
+        bottom: TabBar(
+          controller: tabController,
+          labelColor: colLight,
+          indicatorColor: colPrimary,
+          indicatorWeight: 5,
+          onTap: (v) {
+            setState(() {
+              // model.scrollController.position.setPixels(0);
+              // model.isSearch = false;
+              // model.searchTransaksi.text = "";
+              // model.wordSearch1 = "";
+              // model.wordSearch2 = "";
+              // model.foc_search.unfocus();
+            });
+          },
+          tabs: <Widget>[
+            Tab(
+              text: "Confirm",
+            ),
+            Tab(
+              text: "Pending",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: <Widget>[
+          _buildConfirm(),
+          _buildPending(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildConfirm() {
+    return Container(
+      child: FutureBuilder<List>(
+        future: _getMahasiswa("Confirm"),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text(
+                        snapshot.data[index]['nama_mahasiswa'],
+                        style: GoogleFonts.mcLaren(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle:
+                          Text("NIM :" + snapshot.data[index]['nim_mahasiswa']),
+                    );
+                  },
+                )
+              : loadingCircular();
+        },
+      ),
+    );
+  }
+
+  Widget _buildPending() {
+    return Container(
+      child: FutureBuilder<List>(
+        future: _getMahasiswa("Pending"),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text(
+                        snapshot.data[index]['nama_mahasiswa'],
+                        style: GoogleFonts.mcLaren(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle:
+                          Text("NIM :" + snapshot.data[index]['nim_mahasiswa']),
+                    );
+                  },
+                )
+              : loadingCircular();
+        },
+      ),
+    );
+  }
+}
