a2133e5279c8915ed6d2d489a4c28016b8efb933
Fauzy Wahyudi <fauzy.wahyudi79@gmail.com>
2020-09-05 23:11:47 +0700

    #42 add PengumumanPage dan List Data Pengumuman

--- bimbingan_online/lib/main.dart
+++ bimbingan_online/lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:bimbingan_online/views/dosen/pages/fitur_kaprodi/dosen/dosen_page.dart';
-import 'package:bimbingan_online/views/dosen/pages/fitur_kaprodi/mahasiswa/mahasiswa_page.dart';
-import 'package:bimbingan_online/views/dosen/pages/fitur_kaprodi/proposal/proposal_page.dart';
-import 'package:bimbingan_online/views/login.dart';
+import 'views/dosen/pages/fitur_kaprodi/dosen/dosen_page.dart';
+import 'views/dosen/pages/fitur_kaprodi/mahasiswa/mahasiswa_page.dart';
+import 'views/dosen/pages/fitur_kaprodi/pengumuman/pengumuman_page.dart';
+import 'views/dosen/pages/fitur_kaprodi/proposal/proposal_page.dart';
 import 'package:bimbingan_online/views/splashscreen.dart';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         MahasiswaPage.routeName: (context) => MahasiswaPage(),
         DosenPage.routeName: (context) => DosenPage(),
         ProposalPage.routeName: (context) => ProposalPage(),
+        PengumumanPage.routeName: (context) => PengumumanPage(),
       },
       builder: BotToastInit(),
       theme: ThemeData(
---
+++ bimbingan_online/lib/views/dosen/pages/fitur_kaprodi/pengumuman/pengumuman_page.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'package:bimbingan_online/providers/pengumuman_provider.dart';
+import 'package:bimbingan_online/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PengumumanPage extends StatefulWidget {
+  static const routeName = '/PengumumanPage';
+  @override
+  _PengumumanPageState createState() => _PengumumanPageState();
+}
+
+class _PengumumanPageState extends State<PengumumanPage> {
+  PengumumanProvider _pengumumanProvider = PengumumanProvider();
+
+  Future<List> _getPengumuman(String status) async {
+    final result = await _pengumumanProvider.getPengumuman(context, status);
+    return result;
+  }
+
+  Future<Null> handleRefresh() async {
+    Completer<Null> completer = new Completer<Null>();
+    new Future.delayed(new Duration(milliseconds: 500)).then((_) {
+      completer.complete();
+      setState(() {});
+    });
+    return completer.future;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pengumuman"),
+      ),
+      body: Container(
+        child: FutureBuilder<List>(
+          future: _getPengumuman(""),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) print(snapshot.error);
+            return snapshot.hasData
+                ? RefreshIndicator(
+                    onRefresh: handleRefresh,
+                    child: ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          leading: Icon(Icons.person),
+                          title: Text(
+                            snapshot.data[index]['judul_pengumuman'],
+                            style: GoogleFonts.mcLaren(),
+                          ),
+                          subtitle: Text(snapshot.data[index]['tgl_post']),
+                        );
+                      },
+                    ),
+                  )
+                : loadingCircular();
+          },
+        ),
+      ),
+    );
+  }
+}
